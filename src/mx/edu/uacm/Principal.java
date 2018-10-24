f37729d47ebeb2808f7de3d2ecf094d83ae3f2dd
luisdavid <luisdaviduacm@gmail.com>
2018-10-24 08:16:18 -0500

    Se agrego solo lo de la generacion de una nueva poblacion en ciclo principal.

--- src/mx/edu/uacm/Principal.java
+++ src/mx/edu/uacm/Principal.java
@@ -3,12 +3,13 @@ package mx.edu.uacm;
 public class Principal {
 	public static void main(String[] args) {
 		AlgoritmoGenetico ag= new AlgoritmoGenetico(50, 0.01,0.95,0);
-		Poblacion p= ag.getNuevaPobacion(10);
+		Poblacion p= ag.getNuevaPobacion(8);
+		ag.evaluarPoblacion(p);
 		int generacion=1;
-		/*for(Individuo i: p.getIndividuos()) {
+		for(Individuo i: p.getIndividuos()) {
 			System.out.println(i);
-		}*/
-		while(ag.encontreSolucion(p)) {
+		}
+		while(!ag.encontreSolucion(p)) {
 			System.out.println("Fitness de la poblacion:"+p.getFitnessPoblacion());
 			System.out.println("Mejor solucion: "+p.obtenerIndividuoMasApto(0));
 			//Aplicar cruce
@@ -16,9 +17,12 @@ public class Principal {
 			//Aplicar mutacion
 			
 			//Evaluacion de la poblacion
+			p= ag.getNuevaPobacion(8);
 			ag.evaluarPoblacion(p);
 			
 			generacion++;
+			System.out.println("generacion: "+generacion);
+
 		}
 		System.out.println("Fitness de la poblacion:"+p.getFitnessPoblacion());
 		System.out.println("Mejor solucion: "+p.obtenerIndividuoMasApto(0));
